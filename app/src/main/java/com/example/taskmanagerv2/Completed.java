23f8ed37d9b20017850d4a4706936527061ac048
IAmDanieI <mypratastastegood@gmail.com>
2021-07-06 12:20:58 +0800

    Updated Version

--- app/src/main/java/com/example/taskmanagerv2/Completed.java
+++ app/src/main/java/com/example/taskmanagerv2/Completed.java
@@ -14,6 +14,5 @@ public class Completed extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment1,container,false);
-
     }
 }
